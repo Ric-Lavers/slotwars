9e7b7c67528cc8df1ca56ffd81979d022ec4444f
Terence Jeong <terence.jeong@gmail.com>
2017-10-20 11:05:35 +1100

    changes to README.MD

--- slotwars/db/schema.rb
+++ slotwars/db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20171019063726) do
+ActiveRecord::Schema.define(version: 20171019225751) do
 
   create_table "profiles", force: :cascade do |t|
     t.integer "user_id"
